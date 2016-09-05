4540fb8495503e40933a01fe238d8a03c022afde
crytography <154491070@qq.com>
2016-09-05 13:03:02 +0800

    添加注释
    添加注释

--- 虚函数表/虚函数表.cpp
+++ 虚函数表/虚函数表.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+class H
+{
+
+	virtual void j(void)
+	{
+		cout << "H::j" << endl;
+	}
+
+};
 class A
 {
 	virtual void f(void)
@@ -11,14 +20,29 @@ class A
 		cout << "A::g" << endl;
 	}
 };
-class B :public A
+class B :public A,public H
 {
+	 void f(void)
+	{
+		cout << "B::f" << endl;
+	}
 	virtual void h(void)
 	{
 		cout << "B::h" << endl;
 	}
 };
+typedef void (*fun)(void);//typedef ×÷ÓÃ
 void main()
 {
+	B b;
+	fun pte;
+	for (int i = 0; i < 3; i++)
+	{
+		pte = (fun)*((int*)*(int*)(&b) + i);
+		pte();
+	}
+	pte = (fun)*(int*)(*((int *)&b + 1));
+	pte();
+	system("pause");
 
 }
